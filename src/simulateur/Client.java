9be67644e8d955b3e9b6a1b26013ad8a1ba6d576
FtaehBenBrahim <benbrahimfateh20@gmail.com>
2022-12-31 21:42:41 +0100

    tahraoui

--- src/simulateur/Client.java
+++ src/simulateur/Client.java
@@ -10,29 +10,6 @@ public class Client {
 	 this.serviceStartTime=serviceStartTime;
 	 this.departureTime=departureTime;
 	}
-	public int getArriveTime() {
-		return arriveTime;
-	}
-	public void setArriveTime(int arriveTime) {
-		this.arriveTime = arriveTime;
-	}
-	public int getServiceStartTime() {
-		return serviceStartTime;
-	}
-	public void setServiceStartTime(int serviceStartTime) {
-		this.serviceStartTime = serviceStartTime;
-	}
-	public int getDepartureTime() {
-		return departureTime;
-	}
-	public void setDepartureTime(int departureTime) {
-		this.departureTime = departureTime;
-	}
 	
-	@Override
-	public String toString() {
-		return "Client [arriveTime=" + arriveTime + ", serviceStartTime=" + serviceStartTime + ", departureTime="
-				+ departureTime + "]";
-	}
 	
 }
